baffe70f5bae7b6d6af8b9896e68fcc54da030c0
Roid-obi <contohdiamazon2@gmail.com>
2024-01-04 10:17:15 +0700

    featbutton secondary and card

--- src/stories/Card/Card.stories.tsx
+++ src/stories/Card/Card.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Title, Description, Subtitle, Stories, Primary as PrimaryDocBlock, Controls} from "@storybook/blocks"
 
 import Card from "./Card";
+import Button from "../Button/Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -35,10 +36,22 @@ type Story = StoryObj<typeof meta>;
 export const CardImage: Story = {
   args: {
     img: "https://images.unsplash.com/photo-1482877346909-048fb6477632?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=958&q=80",
-    Content: 
+    Content:
+    <>
       <p style={{ color: "white" }}>
         Earum rerum vero, nostrum, cupiditate repellat quod distinctio facilis repellendus iste ratione reprehenderit architecto est itaque, sequi molestias.
-      </p>,
+      </p>
+      <Button
+        Text="Button"
+        Type="secondary"
+        onClick={function noRefCheck(){}}
+        style={{
+          height: '48px',
+          width: '118px'
+        }}
+      />
+      </>
+      ,
     
     style: { 
       height: "378px", 
